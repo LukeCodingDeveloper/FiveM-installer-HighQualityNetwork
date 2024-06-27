1a9a33b91a544b5452d9a3c2aa360f9ef6235a0f
Luke <87584234+LukeCodingDeveloper@users.noreply.github.com>
2024-06-27 09:43:18 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ runtime_link=$1
 
 status(){
   clear
-  echo -e "${green}$@...${nc}"
+  echo -e $green$@'...'$reset
   sleep 1
 }
 
@@ -17,42 +17,41 @@ runCommand(){
     COMMAND=$1
 
     if [[ ! -z "$2" ]]; then
-      status "$2"
+      status $2
     fi
 
-    eval "$COMMAND";
+    eval $COMMAND;
     BASH_CODE=$?
     if [ $BASH_CODE -ne 0 ]; then
-      echo -e "${red}An error occurred:${nc} ${white}${COMMAND}${nc}${red} returned${nc} ${white}${BASH_CODE}${nc}"
+      echo -e "${red}An error occurred:${reset} ${white}${COMMAND}${reset}${red} returned${reset} ${white}${BASH_CODE}${reset}"
       exit ${BASH_CODE}
     fi
 }
 
-# Einbindung der BashSelect-Funktionalität
 source <(curl -s https://raw.githubusercontent.com/LukeCodingDeveloper/BashSelect.sh/main/BashSelect.sh)
 clear
 
-# Auswahl des Alpine-Verzeichnisses
-status "Select the alpine directory:"
-readarray -t directories <<< $(find / -name "alpine")
-export OPTIONS=("${directories[@]}")
+
+status "Select the alpine directory"
+readarray -t directorys <<<$(find / -name "alpine")
+export OPTIONS=(${directorys[*]})
 
 bashSelect
 
-selected_index=$?
-dir="${directories[$selected_index]}/.."
+dir=${directorys[$?]}/..
 
-# Überprüfen, ob ein Prozess auf Port 40120 läuft
-lsof -i :40120 > /dev/null
-if [[ $? -eq 0 ]]; then
-  status "It looks like something is running on the default TxAdmin port. Can we stop/kill it?" "/"
+
+lsof -i :40120
+if [[ $( echo $? ) == 0 ]]; then
+
+  status "It looks like there is something running on the default TxAdmin port. Can we stop/kill it?" "/"
   export OPTIONS=("Kill PID on port 40120" "Exit the script")
   bashSelect
   case $? in
     0 )
-      status "Killing PID on port 40120"
+      status "killing PID on 40120"
       runCommand "apt -y install psmisc"
-      runCommand "fuser -4 40120/tcp -k"
+	  runCommand "fuser -4 40120/tcp -k"
       ;;
     1 )
       exit 0
@@ -60,36 +59,35 @@ if [[ $? -eq 0 ]]; then
   esac
 fi
 
-# Löschen des alpine-Verzeichnisses
 echo "${red}Deleting ${nc}alpine"
 sleep 1
-runCommand "rm -rf $dir/alpine"
+rm -rf $dir/alpine
 clear
 
-# Löschen der run.sh Datei
 echo "${red}Deleting ${nc}run.sh"
 sleep 1
-runCommand "rm -f $dir/run.sh"
+rm -f $dir/run.sh
 clear
 
-# Herunterladen der fx.tar.xz Datei
 echo "Downloading ${yellow}fx.tar.xz${nc}"
-runCommand "wget --directory-prefix=$dir $runtime_link"
+wget --directory-prefix=$dir $1
 echo "${green}Success${nc}"
+
 sleep 1
 clear
 
-# Entpacken der fx.tar.xz Datei
 echo "Unpacking ${yellow}fx.tar.xz${nc}"
-runCommand "tar xf $dir/fx.tar.xz -C $dir"
+tar xf $dir/fx.tar.xz -C $dir
+
 echo "${green}Success${nc}"
 sleep 1
+
 clear
 
-# Löschen der fx.tar.xz Datei
-runCommand "rm -f $dir/fx.tar.xz"
+rm -r $dir/fx.tar.xz
 echo "${red}Deleting ${nc}fx.tar.xz"
+
 sleep 1
 clear
 
-echo "${green}Update successful${nc}"
+echo "${green}update success${nc}"
